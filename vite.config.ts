9cfc86336c10948dd0fe63079c85e4bee9a192df
Jamile Otaki <89342954+jamiesorko@users.noreply.github.com>
2025-12-27 07:53:54 -0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   resolve: {
+    // This allows you to import files without using .ts or .tsx extensions
     extensions: ['.mjs', '.js', '.ts', '.jsx', '.tsx', '.json']
   },
   define: {
